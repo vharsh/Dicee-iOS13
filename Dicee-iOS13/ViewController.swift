e576fdfc25bd849c45e99c22a4761fcebb47d9b6
Harsh Vardhan <harsh.vardhan1@thoughtworks.com>
2022-11-15 08:59:29 +0530

    feat: change dice on button tap
    Signed-off-by: Harsh Vardhan <harsh.vardhan1@thoughtworks.com>

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rightDice: UIImageView!
+    @IBOutlet weak var leftDice: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // NOTES: we get the WHO and now using the object of it we can decide WHAT to do with that object
+        // typical OOP :)
+        // this can be changed via Attribute inspector too!
+        leftDice.alpha = 0.5
+        // TODO: Change on Roll button click
+        leftDice.image = UIImage(imageLiteralResourceName: "DiceSix")
+        rightDice.image = UIImage(imageLiteralResourceName: "DiceTwo")
     }
 
-
+    @IBAction func rollButton(_ sender: UIButton) {
+        leftDice = UIImage(imageLiteralResourceName: "DiceFour")
+        rightDice = UIImage(imageLiteralResourceName: "DiceFour")
+    }
 }
 
